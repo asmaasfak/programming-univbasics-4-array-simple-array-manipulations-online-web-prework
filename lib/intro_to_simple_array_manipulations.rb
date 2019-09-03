97a90297069e89a89215e00cb99758d37d45c2d2
avid-enhancer-8592 <asma_asfak@yahoo.com>
2019-09-03 03:40:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,15 @@ end
 def pop_with_args (dog_breeds)
   dog_breeds.pop(2)
 end
+
+def using_shift (my_favorite_cities)
+  my_favorite_cities.shift
+end
+
+def shift_with_args (ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
+
+def using_concat (my_favorite_things, more_favs)
+  my_favorite_things.concat(more_favs)
+end
